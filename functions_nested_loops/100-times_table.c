3dab0ff08186e8f89c38c5426bf609f1eb185920
Giftiola <52719101+Giftiola@users.noreply.github.com>
2023-02-19 13:31:31 -0800

    Update 100-times_table.c
    n<= 0 || n>=15

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,10 +9,12 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n >= 0 && n < 15)
+	if (n > 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
+			if (n <= 0 || n>= 15)
+				continue;
 			_putchar('0');
 			for (j = 1; j <= n; j++)
 				putformat(i * j);
